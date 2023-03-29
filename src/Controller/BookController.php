a019a6c2d1d753c5802285465dc0311dc27db8ce
Zheneos <loic.scognamiglio16@gmail.com>
2023-03-29 02:04:44 +0200

    goinSport(init) : First commit

---
+++ src/Controller/BookController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BookController extends AbstractController
+{
+    /**
+     * @Route("/book", name="book")
+     */
+    public function index(): Response
+    {
+        $ConfClient = ['tennis','padel','squash']; // Dans un gros projet, gérer cette date à partir de la configuration du client pour la rendre dynamique et propre au client
+        return $this->render('Book/index.html.twig', [
+            'optionsActivity' => $ConfClient
+        ]);
+    }
+
+    /**
+     * @Route("/postBook", name="postBook")
+     */
+    public function postBook(Request $request)
+    {
+        return "blbl";
+
+
+    }
+}
